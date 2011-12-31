bfb75e126b1a0645cb943c13a84299e544332f4c
dlowashere <dl575@cornell.edu>
2011-12-30 19:02:53 -0500

    Move menu2, ssb2 to menu, ssb. Add instructions for Esc to quit.

--- ssb/src/ssb/menu.py
+++ ssb/src/ssb/menu.py
@@ -1,96 +1,105 @@
 '''
-Created on Dec 26, 2011
+Menu object for Something Somethin Bricks.
 
-Something Something Bricks menu system.
+Created on Dec 27, 2011
 
-@author: Daniel Lo
+@author: Dan
 '''
 
 import pygame
-import sys
-import os
 
 from color import *
 
-def print_surface(msg, surface, topleft, font_obj):
-  """
-  Print the passed msg string to the surface at location specified by topleft.
-  Arguments
-    msg        String to be displayed.
-    surface    Pygame surface to print string to.
-    topleft    (x, y) top left corner of location to print to.
-    font_obj   Pygame font object for text rendering.
-  """
-  # Surface containing game over text
-  # Do not anti-alias, render in white
-  msg_surface = font_obj.render(msg, False, white)
-  # Create rect object to specify where to place text
-  msg_rect = msg_surface.get_rect()
-  msg_rect.topleft = topleft
-  surface.blit(msg_surface, msg_rect)
-
-def menu_execute(menu_state):
-  """
-  Execute the menu item selected.
-    menu_state    Menu item selected.
-  """
-  if menu_state == "Play":
+class Menu:
+  
+  # Directory where fonts are kept
+  _fonts_dir = "fonts/"
+  
+  def __init__(self, topleft = (0, 0), mixer=None):
+    """
+    Default constructor.
+      topleft    (x, y) top left corner of location for menu
+      mixer      Pygame mixer object for sounds
+    """
+    # Top left corner of menu
+    self.left, self.top = self.topleft = topleft
+    # Pixels between top of each line
+    self.linespacing = 30
+    # Which item is selected
+    self.selected = "Play"
     
-    os.system("ssb.py")
-    sys.exit()
-  elif menu_state == "Exit":
-    sys.exit()
+    # Add sounds
+    if mixer:
+      # Directory where sounds are kept
+      snd_dir = "sounds/"
+      # Sound for key press
+      self.snd_key = mixer.Sound(snd_dir + "select.wav")
+      self.snd_key.set_volume(0.1)
+  
+  def draw_menu(self, surface):
+    """
+    Draw menu to the passed surface starting at the topleft corner passed.
+      surface    Surface to draw menu to.
+    """
+    font_obj = pygame.font.Font(self._fonts_dir + "OpenSans-Regular.ttf", 20)
+    topleft = [self.left + 5, self.top]
+    self.print_surface("Play", surface, topleft, font_obj)
+    topleft[1] += self.linespacing
+    self.print_surface("Exit", surface, topleft, font_obj)
+    # Draw selection cursor
+    if self.selected == "Play":
+      pygame.draw.rect(surface, white, pygame.rect.Rect(self.topleft, (100, self.linespacing)), 2)
+    elif self.selected == "Exit":
+      pygame.draw.rect(surface, white, pygame.rect.Rect((self.left, self.top + self.linespacing), (100, self.linespacing)), 2)
+    else:
+      raise Exception("Unknown menu state.")
+    # Print instructions
+    control_linespacing = 18
+    font_obj = pygame.font.Font(self._fonts_dir + "OpenSans-Regular.ttf", 14)
+    topleft[1] += 2*self.linespacing
+    self.print_surface("Z, X to rotate", surface, topleft, font_obj)
+    topleft[1] += control_linespacing
+    self.print_surface("Arrow keys to move", surface, topleft, font_obj)
+    topleft[1] += control_linespacing
+    self.print_surface("M to mute", surface, topleft, font_obj)
+    topleft[1] += control_linespacing
+    self.print_surface("Esc to quit", surface, topleft, font_obj)
+    
+    # Draw rectangle around everything
+    topleft = (self.topleft[0] - 8, self.topleft[1] - 8)
+    size = (160, 180)
+    pygame.draw.rect(surface, gray, pygame.Rect(topleft, size), 2)
+    
+  def menu_up(self):
+    """
+    Move up in the menu selections.
+    """
+    if self.selected == "Exit":
+      self.selected = "Play"
+      self.snd_key.play()
+    
+  
+  def menu_down(self):
+    """
+    Move down in the menu selections.
+    """
+    if self.selected == "Play":
+      self.selected = "Exit"
+      self.snd_key.play()
 
-## Pygame initialization
-# Reduce sound buffer size (4096 to 512) to reduce lag
-pygame.mixer.pre_init(44100, -16, 2, 512)
-# Initialize Pygame    
-pygame.init()
-# Window size
-size = width, height = 370, 180
-# Initialize window
-screen = pygame.display.set_mode(size)
-pygame.display.set_caption('Something Something Bricks')
-# Font object for rendering text
-font_large = pygame.font.Font(None, 36)
-font_small = pygame.font.Font(None, 28)
-
-# Possible menu items
-menu = {"Play":(150, 80), "Exit":(150, 120)}
-# Where the menu cursor is
-menu_state = "Play"
-
-# Main loop
-while True:
-  
-  # Event Handler
-  for event in pygame.event.get():
-    # Keypresses
-    if event.type == pygame.KEYDOWN:
-      # Quit game
-      if event.key == pygame.K_ESCAPE:
-        sys.exit()
-      
-      # Down in menu selection
-      if event.key == pygame.K_DOWN:
-        menu_state = "Exit"
-      elif event.key == pygame.K_UP:
-        menu_state = "Play"
-      elif event.key == pygame.K_RETURN:
-        menu_execute(menu_state)
-  
-  # Blank screen
-  screen.fill(black)      
-  
-  # Game title
-  print_surface("Something Something Bricks", screen, (10, 20), font_large)
-  # Display menu items
-  for key in menu.keys():
-    print_surface(key, screen, menu[key], font_small)
-  # Display rectangle to indicate current selection
-  pygame.draw.rect(screen, white, pygame.rect.Rect(menu[menu_state], (50, 30)), 1)
-  
-  
-  # Update display
-  pygame.display.update()
-  
+  def print_surface(self, msg, surface, topleft, font_obj):
+    """
+    Print the passed msg string to the surface at location specified by topleft.
+    Arguments
+      msg        String to be displayed.
+      surface    Pygame surface to print string to.
+      topleft    (x, y) top left corner of location to print to.
+      font_obj   Pygame font object for text rendering.
+    """
+    # Surface containing game over text
+    # Do not anti-alias, render in white
+    msg_surface = font_obj.render(msg, True, white)
+    # Create rect object to specify where to place text
+    msg_rect = msg_surface.get_rect()
+    msg_rect.topleft = topleft
+    surface.blit(msg_surface, msg_rect)
--- ssb/src/ssb/ssb.py
+++ ssb/src/ssb/ssb.py
@@ -11,114 +11,15 @@ Created on Dec 20, 2011
 import pygame
 import sys
 
-from brick import DoubleBrick
-from brick import Brick
+from board import Board
 from color import *
-
-def spread_break(stacked_bricks, column_top, col, row, breaker_color, score):
-  """
-  Break the brick at col, row and recursively call on surrounding bricks.
-  Arguments:
-    stacked_bricks    Array of brick objects that are stacked
-    column_top        Top pixel of each column
-    col               Column index to spread break
-    row               Row index to spread break
-    breaker_color     Color of break to spread
-    score             Current score
-  """
-  # Replace image with a broken brick
-  #stacked_bricks[col][row].break_brick()
-  stacked_bricks[col][row] = Brick()
-  # Decrease top of column      
-  column_top[col] += bh  
-  # Increment score for each brick destroyed
-  score += 10
-
-  # Spread to surrounding blue bricks
-  if col > 0 and stacked_bricks[col-1][row].color == breaker_color:
-    score = spread_break(stacked_bricks, column_top, col-1, row, breaker_color, score)
-  if col < num_cols-1 and stacked_bricks[col+1][row].color == breaker_color:
-    score = spread_break(stacked_bricks, column_top, col+1, row, breaker_color, score)
-  if row > 0 and stacked_bricks[col][row-1].color == breaker_color:
-    score = spread_break(stacked_bricks, column_top, col, row-1, breaker_color, score)
-  if row < num_rows-1 and stacked_bricks[col][row+1].color == breaker_color:
-    score = spread_break(stacked_bricks, column_top, col, row+1, breaker_color, score)
-
-  return score
-
-def drop_bricks(stacked_bricks, column_top):
-  """
-  Fill in any empty spaces by moving bricks above down.
-  Arguments:
-    stacked_bricks    Array of brick objects already stacked
-    column_top        Top pixel of each column
-  """
-  # Handle each column individual
-  for col in range(num_cols):
-    # For each row, starting from the top
-    for row in range(num_rows - 1):
-      # If this spot has a brick, and the one below is empty
-      if stacked_bricks[col][row].color != black and stacked_bricks[col][row+1].color == black:
-        # Move all bricks above down a space, starting from the bottom
-        for drop_row in range(row, -1, -1):
-          stacked_bricks[col][drop_row].rect.top += bh
-          stacked_bricks[col][drop_row + 1] = stacked_bricks[col][drop_row]
-        # Create an empty brick at the top
-        stacked_bricks[col][0] = Brick()
-        
-def break_bricks(stacked_bricks, column_top, score):
-  """
-  Remove any bricks marked as broken.
-  Arguments:
-    stacked_bricks    Array of brick objects already stacked
-    column_top        Top pixel of each column    
-    score             Current score
-  """
-  # Run at least once
-  broken = True
-  
-  # Repeatedly break (combo) until no more breaks to be found
-  while broken: 
-    # If any bricks were broken
-    broken = False
-    # Scan through array
-    for col in range(num_cols):
-      for row in range(num_rows):
-        # If breaker is touching a brick with the same color
-        if stacked_bricks[col][row].breaker:
-          breaker_color = stacked_bricks[col][row].color
-          if (col > 0 and stacked_bricks[col-1][row].color == breaker_color) or \
-            (col < num_cols-1 and stacked_bricks[col+1][row].color == breaker_color) or \
-            (row > 0 and stacked_bricks[col][row-1].color == breaker_color) or \
-            (row < num_rows-1 and stacked_bricks[col][row+1].color == breaker_color):
-            # Use spread_break to recursively break proper bricks
-            score = spread_break(stacked_bricks, column_top, col, row, breaker_color, score)
-            # Play sound for breaking bricks
-            snd_break.play()
-            # Bricks have been broken
-            broken = True
-          
-    # Collapse bricks from above if any bricks were broken
-    if broken:
-      # Drop bricks to fill up space
-      drop_bricks(stacked_bricks, column_top)
-      
-  return score
+from menu import Menu
 
 ## Size parameters  
 # number of rows and columns in play area
-num_rows = 14
-num_cols = 6
-# Column to generate bricks in (0 indexed)
-gen_col = 3
+board_size = num_cols, num_rows = 6, 15
 # Block width, used to space out objects
-block_size = bw, bh = (32, 32)
-# Min, max position of block's left edge
-max_pos_x = bw*num_cols;
-min_pos_x = bw;
-# Lowest/highest position of block's top edge
-max_pos_y = bh*num_rows;
-min_pos_y = bh;
+brick_size = bw, bh = (32, 32)
 
 ## Pygame initialization
 # Reduce sound buffer size (4096 to 512) to reduce lag
@@ -126,271 +27,150 @@ pygame.mixer.pre_init(44100, -16, 2, 512)
 # Initialize Pygame    
 pygame.init()
 # Window size
-size = width, height = (num_cols+2)*2*bw, (num_rows+2)*bh
+size = width, height = (num_cols + 2)*2*bw, (num_rows - 1)*bh
 # Initialize window
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption('Something Something Bricks')
-# Font object for rendering text
-font_obj = pygame.font.Font(None, 28)
 
 # Clock for keeping track of fps
 fps_clock = pygame.time.Clock()
-
-# Possible brick colors
-brick_colors = [red, blue, green, yellow]
-# Brick fall speed
-fallspeed_slow = 1
-fallspeed_fast = 10
-fallspeed = fallspeed_slow
-
-# Create first brick
-db = DoubleBrick(brick_colors, ((gen_col+1)*bw, bh), block_size)
-bricks_list = [db]
-# First brick in pair 
-b1 = db.brick1
-# Second brick in pair
-b2 = db.brick2
-
-## Game state
-# keep track of top of each column
-column_top = [max_pos_y]*num_cols
-# Array keeping track of stacked bricks (column from left, row from top)
-stacked_bricks = [0]*num_cols
-for i in range(num_cols):
-  stacked_bricks[i] = [Brick()]*num_rows
-# Initialize score
-score = 0
-  
-## Sounds
-# Directory where sounds are kept
-snd_dir = "../../sounds/"
-# Sound for key press
-snd_key = pygame.mixer.Sound(snd_dir + "select.wav")
-# Sound for bricks broken
-snd_break = pygame.mixer.Sound(snd_dir + "break.wav")
-# Sound when brick reaches bottom
-snd_drop = pygame.mixer.Sound(snd_dir + "drop.wav")
-# Background music
-#snd_background = pygame.mixer.Sound(snd_dir + "fkobbe-121111final.wav")
-#snd_background.play(loops=-1)
   
 # Screen shot counter
 screen_shot_count = 0
 
+# State of the game: playing or in menu
+game_state = "menu"
+
+# Create game board object
+game_board = Board(board_size, brick_size, pygame.mixer)
+
+# topleft corner of where to display menu
+menu_topleft = (game_board.max_pos_x + 4*game_board.bw, 2*game_board.bh)
+# Create menu object
+menu_obj = Menu(menu_topleft, pygame.mixer)
+
+# Directory where sounds are kept
+snd_dir = "sounds/"
+# Background music
+snd_background = pygame.mixer.Sound(snd_dir + "ssb_bg.ogg")
+snd_bg_vol = 0.2 # Volume when on
+snd_background.play(loops=-1)
+# Default is not muted
+mute = False
+if mute:
+  snd_background.set_volume(0)
+else:
+  snd_background.set_volume(snd_bg_vol)
+
 # Main loop
 while True:
-    
-  # Which column the falling block is in
-  col_index1 = b1.rect.left/bw - 1
-  col_index2 = b2.rect.left/bw - 1
-  # Edges of brick pair
-  # Note these aren't strictly the pixel edges. The right and bottom are actually shifted in by bw, bh respectively.
-  left_edge = min(b1.rect.left, b2.rect.left)
-  top_edge = min(b1.rect.top, b2.rect.top)
-  bottom_edge = max(b1.rect.top, b2.rect.top)
-  right_edge = max(b1.rect.left, b2.rect.left)
     
   # Event Handler
   for event in pygame.event.get():
     # Keypresses
     if event.type == pygame.KEYDOWN:
+      # Key down handling for all states
       # Quit game
       if event.key == pygame.K_ESCAPE:
         sys.exit()
-        
-      # Move brick left (subtract from x coordinate)
-      elif event.key == pygame.K_LEFT:
-        # Bound by left edge
-        if left_edge > min_pos_x and bottom_edge < column_top[col_index1-1] and top_edge < column_top[col_index2-1]:
-          b1.rect.left -= bw
-          b2.rect.left -= bw
-          snd_key.play()
-        
-      # Move brick right (add to x coordinate)
-      elif event.key == pygame.K_RIGHT:
-        # Bound by right edge
-        if right_edge < max_pos_x and bottom_edge < column_top[col_index1+1] and top_edge < column_top[col_index2+1]:
-          b1.rect.left += bw
-          b2.rect.left += bw
-          snd_key.play()
-        
-      # Increased fall speed when DOWN is held
-      elif event.key == pygame.K_DOWN:
-        fallspeed = fallspeed_fast
-        
-      # On rotation, always first (top) brick that moves
-      # Rotate clockwise
-      elif event.key == pygame.K_e:
-        # If first brick above second
-        if b1.rect.top < b2.rect.top and right_edge < max_pos_x and bottom_edge < column_top[col_index1 + 1]:
-          b1.rect.top += bh
-          b1.rect.left += bw
-          snd_key.play()
-        # first brick below second
-        elif b1.rect.top > b2.rect.top and left_edge > min_pos_x and top_edge < column_top[col_index1 - 1]:
-          b1.rect.top -= bh
-          b1.rect.left -= bw
-          snd_key.play()  
-        # first brick to the right of the second
-        elif b1.rect.left > b2.rect.left and bottom_edge + bh < column_top[col_index1 -1]:
-          b1.rect.top += bh
-          b1.rect.left -= bw
-          snd_key.play()
-        # first brick to the left of the second
-        elif b1.rect.left < b2.rect.left:
-          b1.rect.top -= bh
-          b1.rect.left += bw
-          snd_key.play()
-      # Rotate counter-clockwise
-      elif event.key == pygame.K_q:
-        # If first brick above second
-        if b1.rect.top < b2.rect.top and left_edge > min_pos_x and bottom_edge < column_top[col_index1 - 1]:
-          b1.rect.top += bh
-          b1.rect.left -= bw
-          snd_key.play()
-        # first brick below second
-        elif b1.rect.top > b2.rect.top and right_edge < max_pos_x and top_edge < column_top[col_index1 + 1]:
-          b1.rect.top -= bh
-          b1.rect.left += bw
-          snd_key.play()
-        # first brick to the right of the second
-        elif b1.rect.left > b2.rect.left:
-          b1.rect.top -= bh
-          b1.rect.left -= bw
-          snd_key.play()
-        # first brick to the left of the second
-        elif b1.rect.left < b2.rect.left and bottom_edge + bh < column_top[col_index1 + 1]:
-          b1.rect.top += bh
-          b1.rect.left += bw
-          snd_key.play()     
-        
       # Save a screen cap when I is pressed
       elif event.key == pygame.K_i:
         pygame.image.save(screen, "screenshot_%d.png" % screen_shot_count)
         screen_shot_count += 1
+      # Toggle mute
+      elif event.key == pygame.K_m:
+        # If muted
+        if snd_background.get_volume() == 0:
+          # Change volume to on
+          snd_background.set_volume(snd_bg_vol)
+        # If not muted
+        else:
+          # Mute
+          snd_background.set_volume(0)
+        
+      else:
+        # Key down handling specific to play state
+        if game_state == "play":
+          # Move brick left (subtract from x coordinate)
+          if event.key == pygame.K_LEFT:
+            game_board.move_left()
+          # Move brick right (add to x coordinate)
+          elif event.key == pygame.K_RIGHT:
+            game_board.move_right()
+          # Increased fall speed when DOWN is held
+          elif event.key == pygame.K_DOWN:
+            game_board.speed_up()
+          # On rotation, always first (top) brick that moves
+          # Rotate clockwise
+          elif event.key == pygame.K_x:
+            game_board.rotate_cw()
+          # Rotate counter-clockwise
+          elif event.key == pygame.K_z:
+            game_board.rotate_ccw()
+            
+        # Key down handling specific to menu state
+        elif game_state == "menu":
+          # Move down in menu
+          if event.key == pygame.K_DOWN:
+            menu_obj.menu_down()
+          # Move up in menu
+          elif event.key == pygame.K_UP:
+            menu_obj.menu_up()
+          # Execute selected menu item
+          elif event.key == pygame.K_RETURN:
+            # Play game
+            if menu_obj.selected == "Play":
+              # Change program state to play mode
+              game_state = "play"
+              # Start game
+              game_board.start()
+            # Exit game
+            elif menu_obj.selected == "Exit":
+              sys.exit()
+            else:
+              raise Exception("Unknown menu item.")
+            
+        else:
+          raise Exception("Unknown game state.")            
         
     elif event.type == pygame.KEYUP:
-      # Decrease fall speed once DOWN is released
-      if event.key == pygame.K_DOWN:
-        fallspeed = fallspeed_slow
+      # Key up handling when in play state
+      if game_state == "play":
+        # Decrease fall speed once DOWN is released
+        if event.key == pygame.K_DOWN:
+          game_board.slow_down()
+        
     # Quit game
     elif event.type == pygame.QUIT:
       sys.exit()
       
-    # Update column indices after any possible key presses
-    # Which column the falling block is in
-    col_index1 = b1.rect.left/bw - 1
-    col_index2 = b2.rect.left/bw - 1
-    # Edges of brick pair
-    # Note these aren't strictly the pixel edges.
-    # The right and bottom are actually shifted in by bw, bh respectively.
-    left_edge = min(b1.rect.left, b2.rect.left)
-    top_edge = min(b1.rect.top, b2.rect.top)
-    bottom_edge = max(b1.rect.top, b2.rect.top)
-    right_edge = max(b1.rect.left, b2.rect.left)
-    
   # Blank screen
   screen.fill(black)
   
-  ## Write score
-  # String for score information
-  score_msg = "Score: %d" % score
-  # Surface containing score text
-  score_surface = font_obj.render(score_msg, True, white)
-  # Create rect object to specify where to place text
-  score_rect = score_surface.get_rect()
-  score_rect.topleft = (max_pos_x + 2*bw, 2*bh)
-  screen.blit(score_surface, score_rect)
-  
-  # Draw walls
-  pygame.draw.line(screen, white, (bw, bh), (bw, (num_rows+1)*bh), 1)
-  pygame.draw.line(screen, white, (bw, (num_rows+1)*bh), ((num_cols+1)*bw, (num_rows+1)*bh), 1)
-  pygame.draw.line(screen, white, ((num_cols+1)*bw, bh), ((num_cols+1)*bw, (num_rows+1)*bh), 1)
-  # Generation line
-  pygame.draw.line(screen, white, (bw, bh), ((num_cols+1)*bw, bh))
-  # Game over line
-  pygame.draw.line(screen, gray, (bw, 2*bh), ((num_cols+1)*bw, 2*bh))
-  """
-  # Draw vertical grid lines
-  for x in range(5):
-    pygame.draw.line(screen, gray, ((2+x)*bw, bh), ((2+x)*bw, 21*bh), 1)
-  # Draw horizontal grid lines
-  for y in range(19):
-    pygame.draw.line(screen, gray, (bw, (2+y)*bh), (7*bw, (2+y)*bh), 1)
-  """
+  # Draw board area walls
+  game_board.draw_walls(screen)
 
-  # If the brick pair hasn't reached the bottom,
-  if b1.rect.top < column_top[col_index1] - fallspeed and \
-    b2.rect.top < column_top[col_index2] - fallspeed:
-    # Drop bricks
-    b1.rect.top += fallspeed
-    b2.rect.top += fallspeed
-  # Else one of the bricks has reached the top of a column
-  # In this case, both bricks are dropped
-  else:
-    # Sound for brick reaching bottom
-    snd_drop.play()
-    
-    # Brick that hits a surface first needs to handled first (in case stacked)
-    # Set the one that hits first as b1
-    if b2.rect.top >= column_top[col_index2] - fallspeed:
-      b2, b1 = b1, b2
-      col_index1, col_index2 = col_index2, col_index1
+  # Draw menu
+  if game_state == "menu":
+    menu_obj.draw_menu(screen)
+  elif game_state == "play":
+    # If the game is not over
+    if not game_board.state == "game_over":
+      # Advance game
+      game_board.update()
+    # If the game is over, return to menu
     else:
-      # Don't need to do anything, but check that other brick has hit
-      assert(b1.rect.top >= column_top[col_index1] - fallspeed)
-    # Drop bottom brick to bottom
-    b1.rect.top = column_top[col_index1]
-    
-    # Update top position of the column
-    column_top[col_index1] -= bh
-    # Now the other brick
-    b2.rect.top = column_top[col_index2]
-    column_top[col_index2] -= bh
-    # Save to array
-    stacked_bricks[b1.get_col_index()][b1.get_row_index()] = b1
-    stacked_bricks[b2.get_col_index()][b2.get_row_index()] = b2
-    
-    # Handle breaking and update score
-    score = break_bricks(stacked_bricks, column_top, score)
-    
-    # Create new brick
-    db = DoubleBrick(brick_colors, ((gen_col+1)*bw, bh), block_size)
-    # Add to list of existing bricks
-    bricks_list.append(db)
-    # First brick in pair 
-    b1 = db.brick1
-    # Second brick in pair
-    b2 = db.brick2
-    
-  # Draw falling brick
-  screen.blit(b1.image, b1.rect)
-  screen.blit(b2.image, b2.rect)
-  # Draw stacked bricks
-  for col in range(num_cols):
-    for row in range(num_rows):
-      screen.blit(stacked_bricks[col][row].image, stacked_bricks[col][row].rect)
-      
-  # Check for game over condition
-  if column_top[gen_col] < 3*bh:
-    ## Write Game Over to screen
-    # String for score information
-    gameover_msg = "Game Over"
-    # Surface containing game over text
-    gameover_surface = font_obj.render(gameover_msg, True, white)
-    # Create rect object to specify where to place text
-    gameover_rect = gameover_surface.get_rect()
-    gameover_rect.topleft = (max_pos_x + 2*bw, 4*bh)
-    screen.blit(gameover_surface, gameover_rect)
-    pygame.display.update()
-    
-    # Delay 5 seconds, and then exit game
-    pygame.time.delay(5000)
-    sys.exit()
-
+      game_state = "menu"
+  else:
+    raise Exception("Unknown game state")
+  
+  # Draw falling and stacked bricks
+  game_board.draw_bricks(screen)
+  # Draw score
+  game_board.draw_score(screen)
+  
   # Refresh display
   pygame.display.update()    
   # Ensure 60 FPS
   fps_clock.tick(60)
-  # print fps_clock.get_fps()
   
